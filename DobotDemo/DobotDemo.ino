f5d3037d901d944e5a800003ef344aa7e27d5304
fizikai <justinas.valius@gmail.com>
2023-02-17 21:52:12 +0200

    Add files via upload

---
+++ DobotDemo/DobotDemo.ino
@@ -0,0 +1,214 @@
+/****************************************Copyright(c)*****************************************************
+**                            Shenzhen Yuejiang Technology Co., LTD.
+**
+**                                 http://www.dobot.cc
+**
+**--------------File Info---------------------------------------------------------------------------------
+** File name:           main.cpp
+** Latest modified Date:2016-10-24
+** Latest Version:      V2.0.0
+** Descriptions:        main body
+**
+**--------------------------------------------------------------------------------------------------------
+** Modify by:           Edward
+** Modified date:       2016-11-25
+** Version:             V1.0.0
+** Descriptions:        Modified,From DobotDemoForSTM32
+**--------------------------------------------------------------------------------------------------------
+*********************************************************************************************************/
+#include "stdio.h"
+#include "Protocol.h"
+#include "command.h"
+#include "FlexiTimer2.h"
+
+//Set Serial TX&RX Buffer Size
+#define SERIAL_TX_BUFFER_SIZE 64
+#define SERIAL_RX_BUFFER_SIZE 256
+
+//#define JOG_STICK
+/*********************************************************************************************************
+** Global parameters
+*********************************************************************************************************/
+EndEffectorParams gEndEffectorParams;
+
+JOGJointParams  gJOGJointParams;
+JOGCoordinateParams gJOGCoordinateParams;
+JOGCommonParams gJOGCommonParams;
+JOGCmd          gJOGCmd;
+
+PTPCoordinateParams gPTPCoordinateParams;
+PTPCommonParams gPTPCommonParams;
+PTPCmd          gPTPCmd;
+
+uint64_t gQueuedCmdIndex;
+bool suspausta = false;
+
+float startPosX = 200;       
+float startPosY = 0;      
+float startPosZ = 0;       
+float startPosR = 0;       //Start position when powering up
+
+float currentX = startPosX;     
+float currentY = startPosY;   
+float currentZ = startPosZ;   
+float currentR = startPosR;
+
+
+const byte kCmdSetHOMEParams = 13;
+const byte kCmdHOMECmd = 15;
+const int kDataLength = 21;
+
+Pose robotPose;
+
+/*********************************************************************************************************
+** Function name:       setup
+** Descriptions:        Initializes Serial
+** Input parameters:    none
+** Output parameters:   none
+** Returned value:      none
+*********************************************************************************************************/
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  printf_begin();
+  //Set Timer Interrupt
+  FlexiTimer2::set(100, Serialread);
+  FlexiTimer2::start();
+
+}
+
+/*********************************************************************************************************
+** Function name:       Serialread
+** Descriptions:        import data to rxbuffer
+** Input parametersnone:
+** Output parameters:
+** Returned value:
+*********************************************************************************************************/
+void Serialread()
+{
+  while (Serial1.available()) {
+    uint8_t data = Serial1.read();
+    if (RingBufferIsFull(&gSerialProtocolHandler.rxRawByteQueue) == false) {
+      RingBufferEnqueue(&gSerialProtocolHandler.rxRawByteQueue, &data);
+    }
+  }
+}
+/*********************************************************************************************************
+** Function name:       Serial_putc
+** Descriptions:        Remap Serial to Printf
+** Input parametersnone:
+** Output parameters:
+** Returned value:
+*********************************************************************************************************/
+int Serial_putc( char c, struct __file * )
+{
+  Serial.write( c );
+  return c;
+}
+
+/*********************************************************************************************************
+** Function name:       printf_begin
+** Descriptions:        Initializes Printf
+** Input parameters:
+** Output parameters:
+** Returned value:
+*********************************************************************************************************/
+void printf_begin(void)
+{
+  fdevopen( &Serial_putc, 0 );
+}
+
+/*********************************************************************************************************
+** Function name:       InitRAM
+** Descriptions:        Initializes a global variable
+** Input parameters:    none
+** Output parameters:   none
+** Returned value:      none
+*********************************************************************************************************/
+void InitRAM(void)
+{
+    //Set JOG Model
+    gJOGJointParams.velocity[0] = 100;
+    gJOGJointParams.velocity[1] = 100;
+    gJOGJointParams.velocity[2] = 100;
+    gJOGJointParams.velocity[3] = 100;
+    gJOGJointParams.acceleration[0] = 80;
+    gJOGJointParams.acceleration[1] = 80;
+    gJOGJointParams.acceleration[2] = 80;
+    gJOGJointParams.acceleration[3] = 80;
+
+    gJOGCoordinateParams.velocity[0] = 100;
+    gJOGCoordinateParams.velocity[1] = 100;
+    gJOGCoordinateParams.velocity[2] = 100;
+    gJOGCoordinateParams.velocity[3] = 100;
+    gJOGCoordinateParams.acceleration[0] = 80;
+    gJOGCoordinateParams.acceleration[1] = 80;
+    gJOGCoordinateParams.acceleration[2] = 80;
+    gJOGCoordinateParams.acceleration[3] = 80;
+
+    gJOGCommonParams.velocityRatio = 50;
+    gJOGCommonParams.accelerationRatio = 50;
+   
+    gJOGCmd.cmd = AP_DOWN;
+    gJOGCmd.isJoint = JOINT_MODEL;
+
+  
+
+    //Set PTP Model
+    gPTPCoordinateParams.xyzVelocity = 100;
+    gPTPCoordinateParams.rVelocity = 100;
+    gPTPCoordinateParams.xyzAcceleration = 100;
+    gPTPCoordinateParams.rAcceleration = 100;
+
+    gPTPCommonParams.velocityRatio = 20;
+    gPTPCommonParams.accelerationRatio = 20;
+
+    gPTPCmd.ptpMode = MOVL_XYZ;
+    
+    gQueuedCmdIndex = 0;
+
+//Set initial pose (start position)
+  moveArm(startPosX, startPosY, startPosZ, startPosR);
+  //Griperis_atleisti();
+}
+/*********************************************************************************************************
+** Function name:       loop
+** Descriptions:        Program entry
+** Input parameters:    none
+** Output parameters:   none
+** Returned value:      none
+*********************************************************************************************************/
+
+void loop()
+{
+  InitRAM();
+
+  ProtocolInit();
+
+  SetJOGJointParams(&gJOGJointParams, true, &gQueuedCmdIndex);
+
+  SetJOGCoordinateParams(&gJOGCoordinateParams, true, &gQueuedCmdIndex);
+
+  SetJOGCommonParams(&gJOGCommonParams, true, &gQueuedCmdIndex);
+
+  printf("\r\n======Main Program loop started======\r\n");
+
+  SetPTPCmd(&gPTPCmd, true, &gQueuedCmdIndex);
+  ProtocolProcess();
+  while(true) {
+     moveArm(startPosX, startPosY, startPosZ, startPosR);
+     delay(200);
+     Griperis_atleisti();
+     delay(2000);
+     moveArm(200, 100, 50, 50);
+     delay(200);
+    Griperis_suspausti();
+     delay(2000);
+     moveArm(200, 200, 50, 50);
+     delay(200);
+     Griperis_suspausti();
+     delay(2000);
+  }
+  
+}
+
---
+++ DobotDemo/Mano_Funkcijos.ino
@@ -0,0 +1,82 @@
+void moveArm(float x, float y, float z, float r)
+{
+  gPTPCmd.x = x;
+  gPTPCmd.y = y;
+  gPTPCmd.z = z;
+  gPTPCmd.r = r;
+
+  Serial.print("move to x:"); Serial.print(gPTPCmd.x); Serial.print(" y:"); Serial.print(gPTPCmd.y); Serial.print(" z:"); Serial.println(gPTPCmd.r);
+
+  SetPTPCmd(&gPTPCmd, true, &gQueuedCmdIndex);
+  ProtocolProcess();
+
+  currentX = x;
+  currentY = y;
+  currentZ = z;
+  currentR = r;
+}
+
+void Griperis_suspausti(){
+   SetEndEffectorGripper(true, true, &gQueuedCmdIndex);
+   ProtocolProcess();
+   delay(2000);
+   suspausta == true;
+   
+}
+
+void Griperis_atleisti(){
+    if(suspausta == true){
+      SetEndEffectorGripper(true, false, &gQueuedCmdIndex);  // iskleidziam bet oras dar ijungtas
+      ProtocolProcess();
+      delay(2000);
+      suspausta == false;
+    }
+    SetEndEffectorGripper(false, true, &gQueuedCmdIndex);
+    ProtocolProcess();
+    suspausta == false;
+    delay(2000);
+}
+
+void Siurbtuka(bool state){
+  SetEndEffectorSuctionCup(state, false, &gQueuedCmdIndex);
+  ProtocolProcess();
+  delay(2000);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+void SetHOMEParams(float x, float y, float z, byte cmd) {
+  byte cmd_set_home_params[kDataLength] = {0};
+  cmd_set_home_params[0] = kDataLength;
+  cmd_set_home_params[1] = 0x00;
+  cmd_set_home_params[2] = 0x00;
+  cmd_set_home_params[3] = 0x00;
+  cmd_set_home_params[4] = 0x00;
+  cmd_set_home_params[5] = kCmdSetHOMEParams;
+  memcpy(&cmd_set_home_params[6], &x, sizeof(float));
+  memcpy(&cmd_set_home_params[10], &y, sizeof(float));
+  memcpy(&cmd_set_home_params[14], &z, sizeof(float));
+  cmd_set_home_params[18] = cmd;
+
+  Serial1.write(cmd_set_home_params, kDataLength);
+}
+
+void Home() {
+  float x = 200.0;
+  float y = 0.0;
+  float z = 0.0;
+  byte cmd = 0;
+  SetHOMEParams(x, y, z, cmd);
+
+  byte cmd_home[7] = {0};
+  cmd_home[0] = 7;
+  cmd_home[1] = 0x00;
+  cmd_home[2] = 0x00;
+  cmd_home[3] = 0x00;
+  cmd_home[4] = 0x00;
+  cmd_home[5] = kCmdHOMECmd;
+  cmd_home[6] = 1;
+
+  Serial1.write(cmd_home, 7);
+}
+
